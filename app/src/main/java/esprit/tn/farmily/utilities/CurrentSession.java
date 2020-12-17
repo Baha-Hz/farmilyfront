e99480698825ce23cc6a42ecf3d197d3f016ba3c
ikken05 <mahdi.ikken@esprit.tn>
2020-12-17 06:58:12 +0100

    SignUP yemshi Login zid thabat

---
+++ app/src/main/java/esprit/tn/farmily/utilities/CurrentSession.java
@@ -0,0 +1,11 @@
+package esprit.tn.farmily.utilities;
+
+import esprit.tn.farmily.LoginrRegister.User;
+
+public class CurrentSession {
+
+    public static User CurrentUser = new User();
+    public static String Token;
+
+
+}
